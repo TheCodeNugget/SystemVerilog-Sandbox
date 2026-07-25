641b11e2a70f1505fc7c532ebfce13d16f7b7403
TheCodeNugget <95768094+TheCodeNugget@users.noreply.github.com>
2026-07-25 19:01:55 +0300

    ✨ add sync_lifo

---
+++ Memory/sync_lifo.sv
@@ -0,0 +1,62 @@
+/// --------------------------------------------------------
+/// sync_lifo.sv
+/// Ken The Nugget
+/// Parametrised Synchronous LIFO Memory
+/// --------------------------------------------------------
+
+module sync_lifo #(
+    parameter DATA_WIDTH = 8,
+    parameter DEPTH = 16
+) (
+    input   logic                   clock,
+    input   logic                   reset,
+    input   logic                   write_en,
+    input   logic                   read_en,
+    input   logic [DATA_WIDTH-1:0]  data_in,
+
+    output  logic                   empty,
+    output  logic                   full,
+    output  logic [DATA_WIDTH-1:0]  data_out
+);
+
+    ///------------------------------------
+    /// Internal Signals
+    ///------------------------------------
+
+    logic [$clog2(DEPTH):0] addr_ptr;
+    logic [DATA_WIDTH-1:0]  data_q;
+    logic [DATA_WIDTH-1:0]  lifo_mem [DEPTH-1:0];
+
+    ///------------------------------------
+    /// LIFO Logic
+    ///------------------------------------
+
+    // Address Pointer Handling
+    always_ff @(posedge clock or posedge reset) begin
+        if (reset) addr_ptr <= '0;
+        else begin
+            if (write_en & !full) addr_ptr <= addr_ptr + 1;
+            if (read_en & !empty) addr_ptr <= addr_ptr - 1;
+        end
+    end
+
+    // Write Handling
+    always_ff @(posedge clock) begin
+        if (write_en & !full) lifo_mem[addr_ptr] <= data_in;
+    end
+
+    // Read Handling
+    always_ff @(posedge clock or posedge reset) begin
+        if (reset) data_q <= '0;
+        else if (read_en & !empty) data_q <= lifo_mem[addr_ptr - 1];
+    end
+
+    ///------------------------------------
+    /// Output Assignments
+    ///------------------------------------
+
+    assign empty = addr_ptr == 0;
+    assign full = addr_ptr == (DEPTH - 1);
+    assign data_out = data_q;
+
+endmodule
